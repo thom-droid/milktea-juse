bc389462c96d198f2e16bae6df2ad1db7da0c5c2
thom-window <hsbang.thom@gmail.com>
2023-02-21 13:55:53 +0900

    chore(ci): typo in deploy.sh

--- server/juse/scripts/deploy.sh
+++ server/juse/scripts/deploy.sh
@@ -5,15 +5,15 @@ PROJECT_NAME=juse
 
 echo "> current time: $(date)" >> $REPOSITORY/log/deploy.log
 
-echo "> copy built file.."
+echo "> copy built file.." >> $REPOSITORY/log/deploy.log
 
 cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
-echo "> current application pid"
+echo "> current application pid" >> $REPOSITORY/log/deploy.log
 
 CURRENT_PIDS=$(pgrep -fl juse | grep jar | awk '{print $1}')
 
-echo "> current application pids: ${CURRENT_PIDS[*]}"
+echo "> current application pids: ${CURRENT_PIDS[*]}" >> $REPOSITORY/log/deploy.log
 
 if [ -z "$CURRENT_PIDS" ]; then
   echo "> no application is running."
@@ -26,21 +26,21 @@ else
   done
 fi
 
-echo "> checking new application"
+echo "> checking new application" >> $REPOSITORY/log/deploy.log
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -E '.*SNAPSHOT\.jar$')
 
 #JAR_NAME=$(ls -tr $REPOSITORY/ | grep SNAPSHOT.jar | tail -n 1)
 
-echo "> jar name: $JAR_NAME"
+echo "> jar name: $JAR_NAME" >> $REPOSITORY/log/deploy.log
 
-echo "> grant permission to jar"
+echo "> grant permission to jar" >> $REPOSITORY/log/deploy.log
 
 chmod +x "$JAR_NAME"
 
-echo "> running $JAR_NAME ..."
+echo "> running $JAR_NAME ..." >> $REPOSITORY/log/deploy.log
 
 nohup java -jar \
 -Dspring.profiles.active=prod \
--Dserver.port=80 $REPOSITORY/"$JAR_NAME" > /home/ec2-user/app/tmp/log/nohup.out 2>&1 &
+-Dserver.port=80 "$JAR_NAME" > /home/ec2-user/app/tmp/log/nohup.out 2>&1 &
 
