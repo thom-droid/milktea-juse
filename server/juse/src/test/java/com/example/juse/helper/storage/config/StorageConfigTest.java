831d47f44f13de28dc11c6809f40a9f71236f3e4
thom-window <hsbang.thom@gmail.com>
2023-03-21 10:18:43 +0900

    fix(test): modify test logic, exporting test result step
    - Modify test logic value comparison -> assert throws when there is no dependency
    - Modify gradle.yml to always export test result whether fails or not.

--- server/juse/src/test/java/com/example/juse/helper/storage/config/StorageConfigTest.java
+++ server/juse/src/test/java/com/example/juse/helper/storage/config/StorageConfigTest.java
@@ -2,15 +2,13 @@ package com.example.juse.helper.storage.config;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.context.ConfigurableApplicationContext;
-import software.amazon.awssdk.profiles.ProfileFile;
 
-import java.nio.file.Path;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StorageConfigTest {
 
@@ -25,13 +23,8 @@ class StorageConfigTest {
     @Test
     public void localProfileTest() {
         ConfigurableApplicationContext context = springApplication.run("--spring.profiles.active=local");
-        StorageProfile storageProfile = context.getBean(StorageProfile.class);
-        ProfileFile profileFile = storageProfile.getProfileFile();
-        String actualPath = storageProfile.getProfilePath();
-        String expected = Path.of("C:","Users","thom",".aws","credentials").toString();
-
-        assertNotNull(profileFile);
-        assertEquals(expected, actualPath);
+        assertDoesNotThrow(()-> context.getBean(StorageProfileForLocal.class));
+        assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(StorageProfileForProd.class));
     }
 
 }
