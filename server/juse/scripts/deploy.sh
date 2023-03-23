724f96f14cea371ec9b3ba61612fe0e9dbcaf4e1
Hyeonsu Bang / Thom <79960964+thom-droid@users.noreply.github.com>
2023-03-23 23:21:33 +0900

    fix: correct code in shell (#40)
    - Remove command to kill dhclient

--- server/juse/scripts/deploy.sh
+++ server/juse/scripts/deploy.sh
@@ -10,7 +10,6 @@ REPOSITORY=/home/ec2-user/codedeploy
 cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
 CURRENT_JAVA_PIDS=$(pgrep -fl java | awk '{print $1}')
-CURRENT_DHCLIENT_PIDS=$(pgrep -fl juse | awk '{print $1}')
 
 echo "> current application pids: ${CURRENT_JAVA_PIDS[*]}" >> $REPOSITORY/log/deploy.log
 
@@ -18,19 +17,6 @@ if [ -z "$CURRENT_JAVA_PIDS" ]; then
   echo "> no application is running. new application is being prepared to launch."
 else
   for PID in ${CURRENT_JAVA_PIDS[*]}
-  do
-    echo "> kill -15 $PID"
-    kill -15 "$PID"
-    sleep 5
-  done
-fi
-
-echo "> kill any dhclient with juse."
-
-if [ -z "$CURRENT_DHCLIENT_PIDS" ]; then
-  echo "> no dhclient is running. "
-else
-  for PID in ${CURRENT_DHCLIENT_PIDS[*]}
   do
     echo "> kill -15 $PID"
     kill -15 "$PID"
